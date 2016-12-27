f1c536d71a85ac57e5a63e5a935e01c4def4101a
Alex Moldovan <alexmoldovan96@gmail.com>
2016-12-27 09:28:47 +0200

    quick fix

--- pyser.py
+++ pyser.py
@@ -1,14 +1,14 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
 
-class ServerPy (BaseHTTPRequestHandler):
-    # def _set_headers (self):
-    #     self.send_response(200)
-    #     self.send_header('Content-type', 'text/html')
-    #     self.end_headers()
+class S (BaseHTTPRequestHandler):
+     def _set_headers (self):
+         self.send_response(200)
+         self.send_header('Content-type', 'text/html')
+         self.end_headers()
 
-    # def do_HEAD (self):
-    #     self._set_headers()
+     def do_HEAD (self):
+         self._set_headers()
         
     def do_GET (self):
         self._set_headers()
